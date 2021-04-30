ac212bc4dd05a91f153da8bd1ddc11a292059a78
Thura <saikhae8@gmail.com>
2021-04-30 10:42:09 +0630

    Delete Food

--- admin/delete-food.php
+++ admin/delete-food.php
@@ -1,5 +1,39 @@
 <?php 
     include('../config/constant.php');
     
-    
+    if(isset($_GET['id'])){
+        // Get ID and Image_name
+        $id = $_GET['id'];
+        $image_name = $_GET['image_name'];
+
+        // Remove Image if available
+        if ($image_name != ''){
+            // has to remove image
+            $path = "../images/food/".$image_name;
+            $remove = unlink($path);
+            echo var_dump($remove);
+            echo $image_name;
+            if ($remove == false){
+                $_SESSION['remove'] = "<div class='failure'> Failed to delete image</div>";
+                header("location:".SITEURL."admin/manage-food.php");
+                die();
+            }
+        }
+
+        // Remove food data from database
+        $sql = "DELETE FROM tbl_food WHERE id = $id";
+        $res = mysqli_query($conn, $sql);
+
+        if($res == true){
+            $_SESSION['delete'] = "<div class='success'>Food Deleted Successfully..</div>";
+            header("location:".SITEURL."admin/manage-food.php");
+        }
+
+        // Redirect to manage-food with a message
+        
+    }else{
+        // Redirect to manage-food
+        $_SESSION['delete'] = "<div class='failure'>Failed to delete food...</div>";
+        header("location:".SITEURL."admin/manage-food.php");
+    }
 ?>
--- admin/manage-food.php
+++ admin/manage-food.php
@@ -21,6 +21,16 @@
                     echo $_SESSION['add'];
                     unset($_SESSION['add']);
                 }
+
+                if(isset($_SESSION['delete'])){
+                    echo $_SESSION['delete'];
+                    unset($_SESSION['delete']);
+                }
+
+                if(isset($_SESSION['remove'])){
+                    echo $_SESSION['remove'];
+                    unset($_SESSION['remove']);
+                }
             ?>
             <br /><br /><br/>
             <a href="<?php echo SITEURL;?>admin/add-food.php" class="btn-primary">Add Food</a>
