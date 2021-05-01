f7399e123bbea5340176b2f28331bab041241ae7
Thura <saikhae8@gmail.com>
2021-05-01 12:52:37 +0630

    Finish

--- admin/index.php
+++ admin/index.php
@@ -18,24 +18,42 @@
         }
     ?>
             <br /> <br />
+            <?php
+                $sql = "SELECT count(*) as count FROM tbl_category";
+                $res = mysqli_query($conn, $sql);
+                $count = mysqli_fetch_assoc($res)['count'];
+
+                $sql1 = "SELECT count(*) as count FROM tbl_food";
+                $res1 = mysqli_query($conn, $sql1);
+                $count1 = mysqli_fetch_assoc($res1)['count'];
+
+
+                $sql2 = "SELECT count(*) as count1 FROM tbl_order";
+                $res2 = mysqli_query($conn, $sql2);
+                $count2 = mysqli_fetch_assoc($res2)['count1'];
+
+                $sql3 = "SELECT sum(total) as sum1 FROM tbl_order where status = 'Delivered'";
+                $res3 = mysqli_query($conn, $sql3);
+                $count3 = mysqli_fetch_assoc($res3)['sum1'];
+            ?>
             <div class="clearfix"></div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <h1><?php echo $count; ?></h1>
                 <br />
                 Categories
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <h1><?php echo $count1; ?></h1>
                 <br />
                 Foods
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <h1><?php echo $count2; ?></h1>
                 <br />
                 Orders
             </div>
             <div class="col-4 text-center">
-                <h1>5</h1>
+                <h1>$<?php echo $count3; ?></h1>
                 <br />
                 Revenue Generated
             </div>
